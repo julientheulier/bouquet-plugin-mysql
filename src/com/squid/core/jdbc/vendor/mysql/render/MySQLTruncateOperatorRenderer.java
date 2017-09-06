adba906ccee13a74fa8470791f005e231fcfb9cf
Julien Theulier <julien@squidsolutions.com>
2017-09-06 11:04:43 +0200

    T3176 fix with one arg

--- src/com/squid/core/jdbc/vendor/mysql/render/MySQLTruncateOperatorRenderer.java
+++ src/com/squid/core/jdbc/vendor/mysql/render/MySQLTruncateOperatorRenderer.java
@@ -2,12 +2,12 @@
  * Copyright © Squid Solutions, 2016
  *
  * This file is part of Open Bouquet software.
- *  
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation (version 3 of the License).
  *
- * There is a special FOSS exception to the terms and conditions of the 
+ * There is a special FOSS exception to the terms and conditions of the
  * licenses as they are applied to this program. See LICENSE.txt in
  * the directory of this program distribution.
  *
@@ -30,26 +30,27 @@ import com.squid.core.sql.render.SQLSkin;
 
 /**
  * Ticket #1190 implements some ANSI functions
- * @author loivd 
+ * @author loivd
  * Truncate function for MySQL
  */
-public class  MySQLTruncateOperatorRenderer 
+public class  MySQLTruncateOperatorRenderer
 extends BaseOperatorRenderer
 {
 	public  MySQLTruncateOperatorRenderer() {
 	}
 
+	@Override
 	public String prettyPrint(SQLSkin skin, OperatorDefinition opDef, String[] args) throws RenderingException {
 		String parameter = args.length > 1 ? "parameter" : "parameters";
 		if (args.length != 1 && args.length != 2) {
 			throw new RenderingException(
 					"Have not supported function TRUNCATE with " + args.length
-							+ parameter);
+					+ parameter);
 		}
 		if (args.length == 1) {
-			return "TRUNCATE(" + args[0] + ")";
+			return "TRUNCATE(" + args[0] + ", 0)";
 		} else {
 			return "TRUNCATE(" + args[0] + "," + args[1] + ")";
-		}		
+		}
 	}
 }
